97485b958fb260d49ba07667ea3e41dfa00b7389
Edgar Zambrano <edgarorlandozambrano@gmail.com>
2023-03-09 17:22:33 -0500

    debug

---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+//reasignacion de variables
+var firstName; //undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = 'David'; //declarando / asignando
+lastName = 'Ana'; // reasignando
+console.log(lastName);
+
+var secondName = 'David'; // declarando / asignando
+var secondName = 'Ana'; // reasignando
+console.log(secondName);
+
+// let 
+let fruits = 'Apple' // declarar u asignar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+
+let fruit = 'Banana';
+console.log(fruit);
+
+// const 
+const animal = 'dog'; // declara y asigna
+//animal = 'cat'; // reasignado
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('car');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
